07a90bd3c34481cdafe9361a6bb0fe146d68848b
Stephen <stephengroat@users.noreply.github.com>
2018-12-06 19:15:22 -0800

    Create sniff.py

---
+++ sniff.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import re
+import scapy.all as scapy
+import scapy_http.http
+
+def pkt(pkt):
+    if scapy_http.http.HTTPRequest in pkt:
+        print(re.match(r'(\/.*)\/', pkt['HTTPRequest'].Path.decode("utf-8")))
+
+if __name__ == '__main__':
+    scapy.sniff(filter="tcp and dst port 80 or tcp and src port 80", prn=pkt)
