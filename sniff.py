286cbee8946b7deb9add23cbdaa54cf6179eec69
Stephen G <stephen@egroat.com>
2018-12-06 20:05:41 -0800

    Add in basic caching idea, need to expand

--- sniff.py
+++ sniff.py
@@ -2,14 +2,21 @@
 import re
 import scapy.all as scapy
 import scapy_http.http
+from cachetools import cached, TTLCache
+import random
+
+cache = TTLCache(maxsize=1024, ttl=10)
 
 def pkt(pkt):
     if scapy_http.http.HTTPRequest in pkt:
         matches = re.match('^((\/\w+)\/?)', pkt['HTTPRequest'].Path.decode("utf-8"))
         if matches:
             print(pkt['HTTPRequest'].Host.decode("utf-8") + matches[2])
+            cache[random.randint(0,1024)] = pkt['HTTPRequest'].Host.decode("utf-8") + matches[2]
         else:
             print(pkt['HTTPRequest'].Host.decode("utf-8"))
+            cache[random.randint(0,1024)] = pkt['HTTPRequest'].Host.decode("utf-8")
+        print(cache.currsize)
 
 if __name__ == '__main__':
     scapy.sniff(filter="(tcp and dst port 80) or (tcp and src port 80)", prn=pkt)
