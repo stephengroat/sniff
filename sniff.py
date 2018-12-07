60ab5c1d1daef7b9aae756dc5f86d42ceb10d461
Stephen G <stephen@egroat.com>
2018-12-06 19:51:22 -0800

    Fix bpf filter and regex

--- sniff.py
+++ sniff.py
@@ -5,7 +5,11 @@ import scapy_http.http
 
 def pkt(pkt):
     if scapy_http.http.HTTPRequest in pkt:
-        print(re.match(r'(\/.*)\/', pkt['HTTPRequest'].Path.decode("utf-8")))
+        matches = re.match('^((\/\w+)\/?)', pkt['HTTPRequest'].Path.decode("utf-8"))
+        if matches:
+            print(pkt['HTTPRequest'].Host.decode("utf-8") + matches[2])
+        else:
+            print(pkt['HTTPRequest'].Host.decode("utf-8"))
 
 if __name__ == '__main__':
-    scapy.sniff(filter="tcp and dst port 80 or tcp and src port 80", prn=pkt)
+    scapy.sniff(filter="(tcp and dst port 80) or (tcp and src port 80)", prn=pkt)
